68f445936ffbf7e6a0919e1c6e0eab84d0afc9a5
magozmorgan <morganpsyinfo@gmail.com>
2024-11-05 13:15:28 +0100

    :ambulance: Firebase auth persistence fixed

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,8 +1,8 @@
 import { initializeApp } from "firebase/app";
-import { getReactNativePersistence, initializeAuth, getAuth } from "firebase/auth";
+import { getReactNativePersistence, initializeAuth, getAuth, Auth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBKwReIsdPPqY0sGAXFGNxKApNcMI7aMrY",
@@ -12,18 +12,25 @@ const firebaseConfig = {
   messagingSenderId: "1094068284982",
   appId: "1:1094068284982:web:3550290a8d4411aa947297",
   measurementId: "G-CZ9DG25Z6J",
-  databaseURL: "https://justdoit-a10fc-default-rtdb.europe-west1.firebasedatabase.app"
+  databaseURL: "https://justdoit-a10fc-default-rtdb.europe-west1.firebasedatabase.app",
 };
 
 // Initialisez Firebase App
 const appFirebase = initializeApp(firebaseConfig);
 
-// Utilisez getAuth() pour vérifier si Firebase Auth est déjà initialisé
-const auth = getAuth(appFirebase) || initializeAuth(appFirebase, {
-  persistence: getReactNativePersistence(AsyncStorage),
-});
+// Vérifiez si `initializeAuth` est nécessaire en vérifiant le type d'environnement
+let auth: Auth;
+if (typeof global !== "undefined" && global?.window === undefined) {
+  // Environnement sans fenêtre (React Native), utilisez initializeAuth
+  auth = initializeAuth(appFirebase, {
+    persistence: getReactNativePersistence(AsyncStorage),
+  });
+} else {
+  // Environnement avec fenêtre (Web), utilisez getAuth
+  auth = getAuth(appFirebase);
+}
 
-const firestore = getFirestore(appFirebase); // Firestore
+const firestore = getFirestore(appFirebase);
 const database = getDatabase(appFirebase);
 
 export { appFirebase, auth, firestore, database };
